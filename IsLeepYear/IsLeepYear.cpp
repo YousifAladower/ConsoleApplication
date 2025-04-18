750e34aff999b53075a3ae9e224ef42b3fef4059
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2025-04-18 18:32:54 +0300

    08-algorithms-problem-solving-level-4

---
+++ IsLeepYear/IsLeepYear.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Get leap Year 
+bool IsLeapYear(short year)
+{
+	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+}
+
+int main()
+{
+	int year;
+	cout << "Enter Year: ";
+	cin >> year;
+	if (IsLeapYear(year))
+	{
+		cout << year << " is a leap year." << endl;
+	}
+	else
+	{
+		cout << year << " is not a leap year." << endl;
+	}
+	system("pause>0");
+	return 0;
+}
