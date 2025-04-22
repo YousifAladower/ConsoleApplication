7c2ed9a64d37aaf798f63c0ca2c1fd3e4db154b4
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2025-04-23 00:11:07 +0300

    class clsCalculator

--- Classes and Objects/Classes and Objects.cpp
+++ Classes and Objects/Classes and Objects.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 class clsPerson
 {
@@ -12,11 +13,78 @@ public:
 		return FirstName + " " + LastName;
 	}
 };
+class clsCalculator
+{
+private:
+	int x=0;
+	
+public:
+
+    int Add(int value)
+    {
+        x += value; // Update the private member x
+        Result("Add");
+        return x;
+    }
+
+    int multiply(int value)
+    {
+        x *= value; // Update the private member x
+        Result("Multiply");
+        return x;
+    }
+
+    int divide(int value)
+    {
+        if (value != 0) // Avoid division by zero
+        {
+            x /= value; // Update the private member x
+            Result("Divide");
+        }
+        else
+        {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+        return x;
+    }
+
+    int subtract(int value)
+    {
+        x -= value; // Update the private member x
+        Result("Subtract");
+        return x;
+    }
+
+    int clear()
+    {
+        x = 0; // Reset x to 0
+        Result("Clear");
+        return x;
+    }
+
+    void Result(string funName)
+    {
+        cout << "\nThe result after " << funName << " is: " << x << endl;
+    }
+
+	
+};
 int main()
 {
-	clsPerson Person1;
+
+    clsCalculator calc;
+
+    calc.Add(10);       // Adds 10 to x and prints the result
+    calc.multiply(2);   // Multiplies x by 2 and prints the result
+    calc.subtract(5);   // Subtracts 5 from x and prints the result
+    calc.divide(3);     // Divides x by 3 and prints the result
+    calc.clear();       // Resets x to 0 and prints the result
+
+    system("pause>0");
+    return 0;
+	/*clsPerson Person1;
 	Person1.FirstName = "Mohammed";
 	Person1.LastName = "Abu-Hadhoud";
 	cout << Person1.FullName() << endl;
-	string S1;
+	string S1;*/
 }
