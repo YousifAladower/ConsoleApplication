7972e68cccb6d339aa1cc1eb8dbd1e1231a34c3d
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2025-05-07 16:17:07 +0300

    Show client and show clients balence

--- Bank System/clsBankClient.h
+++ Bank System/clsBankClient.h
@@ -368,6 +368,21 @@ public:
     {
         return _LoadClientsDataFromFile();
     }
+    static float GetTotalBalances()
+    {
+        vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+        double TotalBalances = 0;
+
+        for (clsBankClient Client : vClients)
+        {
+
+            TotalBalances += Client.AccountBalance;
+        }
+
+        return TotalBalances;
+
+    }
 
 
 };
