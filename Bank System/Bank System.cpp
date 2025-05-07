32dc30859d66ed3387e1c726ed31f5b2e0ddb3f3
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2025-05-07 16:01:00 +0300

    add , update and delete clients

--- Bank System/Bank System.cpp
+++ Bank System/Bank System.cpp
@@ -2,37 +2,151 @@
 //ProgrammingAdvices.com
 //Mohmammed Abu-Hadhoud
 #include <iostream>
+#include "clsPerson.h"
 #include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include <iomanip>
 
-int main()
+void ReadClientInfo(clsBankClient& Client)
+{
+    cout << "\nEnter FirstName: ";
+    Client.FirstName = clsInputValidate::ReadString();
 
+    cout << "\nEnter LastName: ";
+    Client.LastName = clsInputValidate::ReadString();
+
+    cout << "\nEnter Email: ";
+    Client.Email = clsInputValidate::ReadString();
+
+    cout << "\nEnter Phone: ";
+    Client.Phone = clsInputValidate::ReadString();
+
+    cout << "\nEnter PinCode: ";
+    Client.PinCode = clsInputValidate::ReadString();
+
+    cout << "\nEnter Account Balance: ";
+    Client.AccountBalance = clsInputValidate::ReadFloatNumber();
+}
+void PrintClientRecordLine(clsBankClient Client)
 {
 
-    clsBankClient Client1 = clsBankClient::Find("A101");
-    if (!Client1.IsEmpty())
-    {
-        cout << "\nClient Found :-)\n";
-    }
+    cout << "| " << setw(15) << left << Client.AccountNumber();
+    cout << "| " << setw(20) << left << Client.FullName();
+    cout << "| " << setw(12) << left << Client.Phone;
+    cout << "| " << setw(20) << left << Client.Email;
+    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+
+}
+
+void ShowClientsList()
+{
+
+    vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(20) << "Client Name";
+    cout << "| " << left << setw(12) << "Phone";
+    cout << "| " << left << setw(20) << "Email";
+    cout << "| " << left << setw(10) << "Pin Code";
+    cout << "| " << left << setw(12) << "Balance";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    if (vClients.size() == 0)
+        cout << "\t\t\t\tNo Clients Available In the System!";
     else
+
+        for (clsBankClient Client : vClients)
+        {
+
+            PrintClientRecordLine(Client);
+            cout << endl;
+        }
+
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+}
+
+
+void AddNewClient()
+{
+    string AccountNumber = "";
+
+    cout << "\nPlease Enter Account Number: ";
+    AccountNumber = clsInputValidate::ReadString();
+    while (clsBankClient::IsClientExist(AccountNumber))
     {
-        cout << "\nClient Was not Found :-(\n";
+        cout << "\nAccount Number Is Already Used, Choose another one: ";
+        AccountNumber = clsInputValidate::ReadString();
     }
 
-    Client1.Print();
+    clsBankClient NewClient = clsBankClient::GetAddNewClientObject(AccountNumber);
 
-    clsBankClient Client2 = clsBankClient::Find("A101", "1234");
-    if (!Client2.IsEmpty())
+
+    ReadClientInfo(NewClient);
+
+    clsBankClient::enSaveResults SaveResult;
+
+    SaveResult = NewClient.Save();
+
+    switch (SaveResult)
     {
-        cout << "\nClient Found :-)\n";
-    }
-    else
+    case  clsBankClient::enSaveResults::svSucceeded:
     {
-        cout << "\nClient Was not Found :-(\n";
+        cout << "\nAccount Addeded Successfully :-)\n";
+        NewClient.Print();
+        break;
     }
-    Client2.Print();
+    case clsBankClient::enSaveResults::svFaildEmptyObject:
+    {
+        cout << "\nError account was not saved because it's Empty";
+        break;
+
+    }
+    case clsBankClient::enSaveResults::svFaildAccountNumberExists:
+    {
+        cout << "\nError account was not saved because account number is used!\n";
+        break;
+
+    }
+    }
+}
+
+int main()
+{
+    ShowClientsList();
+
+    //clsBankClient Client1 = clsBankClient::Find("A101");
+    //if (!Client1.IsEmpty())
+    //{
+    //    cout << "\nClient Found :-)\n";
+    //}
+    //else
+    //{
+    //    cout << "\nClient Was not Found :-(\n";
+    //}
+
+    //Client1.Print();
+
+    //clsBankClient Client2 = clsBankClient::Find("A101", "1234");
+    //if (!Client2.IsEmpty())
+    //{
+    //    cout << "\nClient Found :-)\n";
+    //}
+    //else
+    //{
+    //    cout << "\nClient Was not Found :-(\n";
+    //}
+    //Client2.Print();
 
 
-    cout << "\nIs Client Exist? " << clsBankClient::IsClientExist("A101");
+    //cout << "\nIs Client Exist? " << clsBankClient::IsClientExist("A101");
 
 
     system("pause>0");
